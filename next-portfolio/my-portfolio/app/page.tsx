be7bf7ca0ba3e838581827561a76dbb6af997b83
salihtzc <salihtzc.1997@gmail.com>
2024-06-11 14:46:46 +0200

    homepage

--- next-portfolio/my-portfolio/app/page.tsx
+++ next-portfolio/my-portfolio/app/page.tsx
@@ -5,30 +5,68 @@ import {BsArrowRight, BsLinkedin} from "react-icons/bs";
 import {HiDownload} from "react-icons/hi";
 import {FaGithubSquare, FaInstagram} from "react-icons/fa";
 import { motion } from "framer-motion";
+import React from "react";
+import Image from 'next/image';
+
 
 
 export default function Home() {
+
+    const fadeIn = (direction = 'down', delay = 0.3) => {
+        return {
+            hidden: {
+                opacity: 0,
+                y: direction === 'down' ? -50 : 50
+            },
+            show: {
+                opacity: 1,
+                y: 0,
+                transition: {
+                    duration: 0.6,
+                    delay: delay,
+                    ease: 'easeInOut'
+                }
+            }
+        };
+    };
+
+
     return (
         <>
             <main className="w-screen h-screen relative">
-                <div className="flex items-center w-full h-full bg-cover bg-center" style={{backgroundImage : "url(/main-bg.webp"}}>
-                    <div className="pl-20 md:pl-40 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]" >
+                <div className="flex items-center justify-between w-full h-full bg-cover bg-center bg-slate-900 ">
+                    <div className="col-span-8 pl-20 md:pl-40 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]" >
 
-                        <h1 className="text-[50px] text-white font-semibold">
+                        <motion.h1
+                            variants={fadeIn('down', 0.2)}
+                            initial='hidden'
+                            whileInView={'show'}
+                            viewport={{once:false,amount:0.4}}
+                            className="text-[50px] text-white font-semibold">
                             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
                               {" "}
                               Jr. Frontend Developer
                                 Mehmet Salih Tuzcu
                             </span>
-                        </h1>
+                        </motion.h1>
 
-                        <p className="text-gray-200 hidden md:block">
+                        <motion.p
+                            variants={fadeIn('down', 0.3)}
+                            initial='hidden'
+                            whileInView={'show'}
+                            viewport={{once:false,amount:0.4}}
+
+                            className="text-gray-200 hidden md:block">
                             Web Development student at Thomas More University of Applied Sciences in Antwerp, Belgium
 
-                        </p>
+                        </motion.p>
 
                         <motion.div
-                            initial="initial"
+                            variants={fadeIn('down', 0.4)}
+                            initial='hidden'
+                            whileInView={'show'}
+                            viewport={{once:false,amount:0.4}}
+                            // initial="initial"
                             animate="animate"
                             className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
                         >
@@ -82,6 +120,19 @@ export default function Home() {
 
 
                     </div>
+
+                    <motion.div
+                        variants={fadeIn('up', 0.3)}
+                        initial='hidden'
+                        whileInView={'show'}
+                        viewport={{once:false,amount:0.1}}
+
+                        className=" hidden lg:flex col-8 offset-2 col-lg-4  offset-lg-0 order-1 order-lg-2 mt-5 pr-10 lg:pr-20" >
+                        <img src='/profilefoto.jpg' alt="Profile" className="img-fluid"
+                             style={{border: 'none', borderRadius: '10%', height: 500, width: 400 }}/>
+                    </motion.div>
+
+
                 </div>
             </main>
 
