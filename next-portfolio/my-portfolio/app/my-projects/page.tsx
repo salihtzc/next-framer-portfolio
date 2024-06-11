7a3bd7ae389290ea491ca3b98b20fd811ffd3a66
salihtzc <salihtzc.1997@gmail.com>
2024-06-11 14:45:51 +0200

    project page background

--- next-portfolio/my-portfolio/app/my-projects/page.tsx
+++ next-portfolio/my-portfolio/app/my-projects/page.tsx
@@ -5,7 +5,7 @@ import ProjectCard from "@/components/ProjectCard";
 export default function Page() {
 
     return(
-        <div className="w-screen h-screen flex items-center justify-center bg-center bg-cover" style={{ paddingTop: '100px' , backgroundImage: "url(/bg-2.jpg)" }}>
+        <div className="w-screen h-screen flex items-center justify-center bg-center bg-cover bg-slate-900" style={{ paddingTop: '100px'}}>
             <div className="grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%] mb-5">
                 {Projects.map((project, index) => (
                     <ProjectCard
