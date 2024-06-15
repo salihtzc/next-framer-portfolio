613721f6d651c2fd84c460b9a45a2fcac5ffe986
salihtzc <salihtzc.1997@gmail.com>
2024-06-15 02:40:08 +0200

    projects

--- next-portfolio/my-portfolio/app/my-projects/page.tsx
+++ next-portfolio/my-portfolio/app/my-projects/page.tsx
@@ -1,21 +1,77 @@
+"use client"
 import React from 'react';
 import {Projects} from "@/constants/data";
 import ProjectCard from "@/components/ProjectCard";
+import { motion } from "framer-motion";
 
 export default function Page() {
+    const fadeIn = (direction = 'down', delay = 0.3) => {
+        let xValue = 0;
+        let yValue = 0;
+
+        switch (direction) {
+            case 'down':
+                yValue = 50;
+                break;
+            case 'up':
+                yValue = -50;
+                break;
+            case 'left':
+                xValue = -50;
+                break;
+            case 'right':
+                xValue = 50;
+                break;
+            default:
+                yValue = 0;
+        }
+
+        return {
+            hidden: {
+                opacity: 0,
+                x: xValue,
+                y: yValue
+            },
+            show: {
+                opacity: 1,
+                x: 0,
+                y: 0,
+                transition: {
+                    duration: 0.6,
+                    delay: delay,
+                    ease: 'easeInOut'
+                }
+            }
+        };
+    };
+
+    const getAnimationDirection = (index) => {
+        // İndex çift sayı ise 'right', tek sayı ise 'left'
+        return index % 2 === 0 ? 'left' : 'right';
+    };
 
     return(
         <div className="w-screen h-screen flex items-center justify-center bg-center bg-cover bg-slate-900" style={{ paddingTop: '100px'}}>
-            <div className="grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%] mb-5">
+
+            <motion.div
+                // variants={fadeIn('left', 0.3)}
+                initial='hidden'
+                whileInView={'show'}
+                viewport={{once:false,amount:0.1}}
+                className="grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%] mb-5">
                 {Projects.map((project, index) => (
-                    <ProjectCard
+                    <motion.div
                         key={index}
-                        title={project.title}
-                        text={project.text}
-                        image={project.src}
-                    />
+                        variants={fadeIn(getAnimationDirection(index), 0.3)}>
+                        <ProjectCard
+                            title={project.title}
+                            text={project.text}
+                            image={project.src}
+                        />
+                    </motion.div>
                 ))}
-            </div>
+            </motion.div>
+
         </div>
     )
 }
