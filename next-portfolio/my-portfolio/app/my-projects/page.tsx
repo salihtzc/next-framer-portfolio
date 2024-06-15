74f5c9231709424c6425eaefc36316904cf59c2f
salihtzc <salihtzc.1997@gmail.com>
2024-06-15 17:50:48 +0200

    my projects page

--- next-portfolio/my-portfolio/app/my-projects/page.tsx
+++ next-portfolio/my-portfolio/app/my-projects/page.tsx
@@ -1,26 +1,31 @@
-"use client"
+"use client";
 import React from 'react';
-import {Projects} from "@/constants/data";
+import { Projects } from "@/constants/data";
 import ProjectCard from "@/components/ProjectCard";
 import { motion } from "framer-motion";
 
 export default function Page() {
+
     const fadeIn = (direction = 'down', delay = 0.3) => {
         let xValue = 0;
         let yValue = 0;
 
         switch (direction) {
-            case 'down':
-                yValue = 50;
-                break;
-            case 'up':
+            case 'downright':
+                xValue = 50;
                 yValue = -50;
                 break;
-            case 'left':
+            case 'upleft':
                 xValue = -50;
+                yValue = 50;
                 break;
-            case 'right':
+            case 'downleft':
+                yValue = 50;
                 xValue = 50;
+                break;
+            case 'upright':
+                yValue = -50;
+                xValue = -50;
                 break;
             default:
                 yValue = 0;
@@ -46,23 +51,31 @@ export default function Page() {
     };
 
     const getAnimationDirection = (index) => {
-        // İndex çift sayı ise 'right', tek sayı ise 'left'
-        return index % 2 === 0 ? 'left' : 'right';
+        switch (index) {
+            case 0:
+                return 'upright';
+            case 1:
+                return 'downright';
+            case 2:
+                return 'upleft';
+            case 3:
+                return 'downleft';
+            default:
+                return 'down';
+        }
     };
 
-    return(
-        <div className="w-screen h-screen flex items-center justify-center bg-center bg-cover bg-slate-900" style={{ paddingTop: '100px'}}>
-
+    return (
+        <div className="w-screen h-screen flex items-center justify-center bg-center bg-cover bg-slate-900" style={{ paddingTop: '100px' }}>
             <motion.div
-                // variants={fadeIn('left', 0.3)}
                 initial='hidden'
-                whileInView={'show'}
-                viewport={{once:false,amount:0.1}}
+                animate='show'
+                viewport={{ once: false, amount: 0.1 }}
                 className="grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%] mb-5">
                 {Projects.map((project, index) => (
                     <motion.div
                         key={index}
-                        variants={fadeIn(getAnimationDirection(index), 0.3)}>
+                        variants={fadeIn(getAnimationDirection(index), index * 0.2)}>
                         <ProjectCard
                             title={project.title}
                             text={project.text}
@@ -71,7 +84,6 @@ export default function Page() {
                     </motion.div>
                 ))}
             </motion.div>
-
         </div>
     )
 }
